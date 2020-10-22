83ac8830c897c27c27b4a0d4a1c79ba981afd4a8
viralsir <viralsir2018@gmail.com>
2020-10-22 13:06:16 +0530

    i have added newyare app and make some changes into hello apps

--- web_dev/hello/urls.py
+++ web_dev/hello/urls.py
@@ -2,5 +2,9 @@ from django.urls import  path
 from . import views
 
 urlpatterns=[
-    path("",views.index,name="home")
+    path("",views.index,name="home"),
+    path("vimal",views.vimal,name="vimal"),
+    path("amit",views.amit,name="amit"),
+    path("greetings/<str:name>",views.greetings,name="greet")
+
 ]
--- web_dev/hello/views.py
+++ web_dev/hello/views.py
@@ -4,3 +4,13 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("hello world !")
+
+def vimal(request):
+    return HttpResponse("Hello Vimal !")
+
+def amit(request):
+    return HttpResponse("Hello Amit !")
+
+def greetings(request,name):
+   # return HttpResponse(f"Hello {name.upper()} !")
+    return render(request,"hello\home.html",{'name':name})
