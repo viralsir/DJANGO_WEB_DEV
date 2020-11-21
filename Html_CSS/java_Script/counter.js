81e962df41302f07818a5f1a942ebfcd0837ff7e
viralsir <viralsir2018@gmail.com>
2020-11-21 18:15:03 +0530

    more javascript added to the projects.and also use localstorage

--- Html_CSS/java_Script/counter.js
+++ Html_CSS/java_Script/counter.js
@@ -1,9 +1,15 @@
- let counter=0
+if(!localStorage.getItem('count'))
+{
+    localStorage.setItem('count',0);
+}
+//let counter=0
         function count()
         {
+            let counter=parseInt(localStorage.getItem('count'));
             counter+=1;
             document.querySelector("h1").innerHTML=counter;
 
+            localStorage.setItem('count ',counter);
             if (counter %10 == 0)
             {
                 alert(`counter reach ${counter}`)
@@ -13,6 +19,7 @@
 
 
         document.addEventListener('DOMContentLoaded',function(){
-            document.querySelector('button').onclick=count;
-        }
-            );
+            //document.querySelector('button').onclick=count;
+            document.querySelector("h1").innerHTML=localStorage.getItem('count');
+            setInterval(count,1000);
+        });
